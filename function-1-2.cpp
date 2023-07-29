dd378ddee44cc2f937de68ce5a536e6b4ebaa2bb
ccnbklsyyyyyyy <140380950+ccnbklsyyyyyyy@users.noreply.github.com>
2023-07-29 22:57:51 +0930

    Update function-1-2.cpp

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -1,4 +1,6 @@
 // function-1-2.cpp
+int array_sum(int array[], int n); // Forward declaration of array_sum function
+
 double array_mean(int array[], int n) {
     if (n < 1) {
         return 0.0;
@@ -7,4 +9,3 @@ double array_mean(int array[], int n) {
     int sum = array_sum(array, n); // Reuse the previously defined array_sum function
     return static_cast<double>(sum) / n;
 }
-
